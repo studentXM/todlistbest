3744d3386ead222a28f03f8f3e2d045da8a99502
vue后台管理系统 <2953282238@qq.com>
2022-07-07 11:04:06 +0800

    修复bug完善功能

--- src/components/MyList.vue
+++ src/components/MyList.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="mylist" @click="test">
+  <div class="mylist">
     <el-table
       :data="list"
       :show-header="false"
       ref="listtable"
       @selection-change="changebox"
-      :row-key="getRowKeys"
     >
-      <el-table-column
-        prop="checked"
-        label="勾选"
-        type="selection"
-        :reserve-selection="true"
-      >
+      <el-table-column prop="checked" label="勾选" type="selection">
         <template slot-scope="scope">
           <el-checkbox
             v-model="scope.row.checked"
@@ -22,10 +16,16 @@
       </el-table-column>
 
       <el-table-column prop="name" label="名称">
-          <template slot-scope="scope">  
-            <el-input v-if="shownow == scope.row.name" size="mini" v-model="nowinp" @blur="savevalue(scope)" v-focus></el-input>
-            <span v-show="shownow != scope.row.name">{{scope.row.name}}</span>
-          </template>
+        <template slot-scope="scope">
+          <el-input
+            v-if="shownow == scope.row.name"
+            size="mini"
+            v-model="nowinp"
+            @blur="savevalue(scope)"
+            v-focus
+          ></el-input>
+          <span v-show="shownow != scope.row.name">{{ scope.row.name }}</span>
+        </template>
       </el-table-column>
 
       <el-table-column>
@@ -62,56 +62,63 @@ export default {
     return {
       list: this.listLabel,
       selected: [],
-      nowinp:'',
-      shownow:'',
+      nowinp: "",
+      shownow: "",
     };
   },
   components: {},
   computed: {},
+  // 自动聚焦 当点击编辑后 显示input就自动聚焦
   directives: {
     focus: {
       // 指令的定义
       inserted: (el) => {
         // 聚焦元素
-        el.querySelector('input').focus()
-        el.focus()
-      }
-    }
+        el.querySelector("input").focus();
+        el.focus();
+      },
+    },
   },
   methods: {
-    test() {
-      this.$bus.$emit("test", "传递成功");
-    },
-
+    // 
     add(e) {
       console.log(e);
       this.selected.push(e);
     },
-    choce(e) {
-      if (e) {
-        this.$refs.listtable.toggleAllSelection();
-      } else {
-        this.$refs.listtable.clearSelection();
+    // 全选
+    choce(x) {
+      if (x) {
+        this.list.forEach((e,i)=>{
+          if(e.checked == false){
+            this.list[i].checked = true
+          }
+        })
+      } else{
+        this.list.forEach((e,i)=>{
+          if(e.checked == true){
+            this.list[i].checked = false
+          }
+        })
       }
     },
     changebox(e) {
       this.selected = e;
     },
+    // 删除
     deletlist(index) {
       this.list.splice(index.$index, 1);
     },
-    getRowKeys(e) {
-      return e.name;
+    // 点击之后展示input回显对应list里的name值
+    showoption(e) {
+      this.shownow = e.row.name;
+      this.nowinp = e.row.name;
     },
-    showoption(e){
-      this.shownow = e.row.name
-      this.nowinp = e.row.name
-    },
-    savevalue(e){
-      // alert()
+    // 失焦之后保存值 赋值给list里面的name
+    savevalue(e) {
       this.list[e.$index].name = this.nowinp;
-      this.shownow = ''
+      this.shownow = "";
     },
+    // 删除选中
     cleardone() {
       // 循环判断再删除时 索引值应该在修改原数组之后 进行对应的修改 才能匹配进度
       // forEach 会跟不上删除的进度 即使修改了 索引值 还是会继续前进 所以这里使用普通for循环
